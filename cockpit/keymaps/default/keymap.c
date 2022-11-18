c9672b9c3844a7ea615cd7736a56cd6db7d6c121
ozkancelik <ozkan44celik@gmail.com>
2022-11-18 17:05:45 +0300

    Update: Cockpit

--- cockpit/keymaps/default/keymap.c
+++ cockpit/keymaps/default/keymap.c
@@ -1,5 +1,18 @@
-// ÖZKAN ÇELIK
-// ozkancelik.com
+/* Copyright 2022 Özkan Çelik -- (github.com/ozkan)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 #include QMK_KEYBOARD_H
@@ -31,10 +44,10 @@ enum cockpit_keycodes {
 #define ADJUST MO(_ADJUST)
 
 #define LSFT_CAPS  LSFT_T(KC_CAPS)
-#define RSFT_ENT   RSFT_T(KC_ENT)
+#define RSFT_ENT   RSFT_T(KC_TAB)
 
 #define FNR_SPC     LT(FNR, KC_SPC)
-#define FNL_ENT     LT(FNL, KC_ENT)
+#define FNL_ENT     LT(FNL, KC_SPC)
 #define ARROW_T LT(ARROW, KC_T)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -57,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                                          ╰──────┴──────┴──────╯
 
   [_COLEMAK] = LAYOUT_cockpit(
-    KC_TAB,     TR_Q,          TR_W,          TR_F,          TR_P,     TR_G,             TR_J,  TR_L,  TR_U,             TR_Y,            TR_SCLN,   KC_BSPC,
+    KC_ESC,     TR_Q,          TR_W,          TR_F,          TR_P,     TR_G,             TR_J,  TR_L,  TR_U,             TR_Y,            TR_SCLN,   KC_BSPC,
     KC_LCTRL,   TR_A,          TR_R,          TR_S,          ARROW_T,  TR_D,             TR_H,  TR_N,  TR_E,             TR_IDOT,         TR_O,      TR_QUOT,
-    LSFT_CAPS,  LALT_T(TR_Z),  LCTL_T(TR_X),  LSFT_T(TR_C),  TR_V,     TR_B,             TR_K,  TR_M,  RSFT_T(TR_COMM),  RCTL_T(TR_DOT),  TR_SLSH,   RSFT_ENT,
+    LSFT_CAPS,  LALT_T(TR_Z),  LCTL_T(TR_X),  LSFT_T(TR_C),  TR_V,     TR_B,             TR_K,  TR_M,  RSFT_T(TR_COMM),  RCTL_T(TR_DOT),  TR_SLSH,   KC_ENT,
                                                                           KC_MPLY,   KC_MUTE,
                                          KC_LALT,   LOWER,    FNL_ENT,                           FNR_SPC,   RAISE,   KC_RGUI,    
                                                                                  KC_UP,
@@ -87,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAKDH] = LAYOUT_cockpit(
     KC_TAB,     TR_Q,          TR_W,          TR_F,          TR_P,     TR_B,             TR_J,  TR_L,  TR_U,             TR_Y,            TR_SCLN,   KC_BSPC,
     KC_LCTRL,   TR_A,          TR_R,          TR_S,          ARROW_T,  TR_G,             TR_M,  TR_N,  TR_E,             TR_IDOT,         TR_O,      TR_QUOT,
-    LSFT_CAPS,  LALT_T(TR_Z),  LCTL_T(TR_X),  LSFT_T(TR_C),  TR_D,     TR_V,             TR_K,  TR_H,  RSFT_T(TR_COMM),  RCTL_T(TR_DOT),  TR_SLSH,   RSFT_ENT,
+    LSFT_CAPS,  LALT_T(TR_Z),  LCTL_T(TR_X),  LSFT_T(TR_C),  TR_D,     TR_V,             TR_K,  TR_H,  RSFT_T(TR_COMM),  RCTL_T(TR_DOT),  TR_SLSH,   KC_ENT,
                                                                           KC_MPLY,   KC_MUTE,
                                          KC_LALT,   LOWER,    FNL_ENT,                           FNR_SPC,   RAISE,   KC_RGUI,    
                                                                                  KC_UP,
@@ -130,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,   _______,  _______,   _______,               TR_LCBR,  TR_RCBR,   TR_UNDS,  TR_DLR,   TR_HASH,  TR_DQUO,
     _______,  _______,  _______,   _______,  _______,   _______,               TR_LBRC,  TR_RBRC,   TR_LABK,  TR_RABK,  TR_BSLS,  KC_END,
                                                                KC_MPRV,   KC_MNXT,
-                              KC_LALT,   LOWER,    FNL_ENT,                           FNR_SPC,   RAISE,   KC_RGUI,    
+                              KC_LALT,   LOWER,    _______,                          KC_ENT,   RAISE,   KC_RGUI,    
                                                                       KC_UP,
                                                          KC_LEFT,    KC_DOWN,  KC_RGHT
 
@@ -139,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
 // RAISE
 // ╭──────┬──────┬──────┬──────┬──────┬──────╮                   ╭──────┬──────┬──────┬──────┬──────┬──────╮
-// │ ESC  │  /   │  1   │  2   │  3   │  -   │                   │  F1  │  F2  │  F3  │  F4  │      │      │
+// │ ESC  │  /   │  1   │  2   │  3   │  -   │                   │  F1  │  F2  │  F3  │  F4  │      │ DEL  │
 // ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
 // │      │  *   │  4   │  5   │  6   │  +   │                   │  F5  │  F6  │  F7  │  F8  │      │      │
 // ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
@@ -157,7 +170,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    
 
   [_RAISE] = LAYOUT_cockpit(
-    KC_ESC,   TR_SLSH,  TR_1,  TR_2,  TR_3,  TR_MINS,          KC_F1,  KC_F2,   KC_F3,   KC_F4,   _______,  _______,
+    KC_ESC,   TR_SLSH,  TR_1,  TR_2,  TR_3,  TR_MINS,          KC_F1,  KC_F2,   KC_F3,   KC_F4,   _______,  KC_DEL,
     _______,  TR_ASTR,  TR_4,  TR_5,  TR_6,  TR_PLUS,          KC_F5,  KC_F6,   KC_F7,   KC_F8,   _______,  _______,
     _______,  TR_PERC,  TR_7,  TR_8,  TR_9,  TR_0,             KC_F9,  KC_F10,  KC_F11,  KC_F12,  _______,  _______,
                                                 KC_MPLY,   KC_MUTE,
@@ -170,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
 // FNR 
 // ╭──────┬──────┬──────┬──────┬──────┬──────╮                   ╭──────┬──────┬──────┬──────┬──────┬──────╮
-// │ ESC  │      │      │  €   │  £   │  Ğ   │                   │      │      │      │      │      │      │
+// │ ESC  │      │      │  €   │  £   │  Ğ   │                   │      │      │      │      │      │ DEL  │
 // ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
 // │      │  !   │  @   │  Ş   │  ₺   │      │                   │      │      │      │      │      │      │
 // ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
@@ -187,7 +200,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_FNR] = LAYOUT_cockpit(
-    KC_ESC,   _______,  _______,  TR_LIRA,  TR_EURO,  TR_GBRV,               _______,  _______,  _______,  _______,  _______,  _______,
+    KC_ESC,   _______,  _______,  TR_LIRA,  TR_EURO,  TR_GBRV,               _______,  _______,  _______,  _______,  _______,  KC_DEL,
     _______,  TR_EXLM,  TR_AT,    TR_SCED,  _______,  TR_PND,                _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  TR_CCED,  _______,  _______,               _______,  _______,  _______,  _______,  _______,  _______,
                                                           KC_MPLY,   KC_MUTE,
@@ -220,7 +233,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,  _______,  _______,  _______,               _______,  _______,  S(TR_I),  TR_I,      TR_ODIA,  TR_GRV,
     _______,  _______,  _______,  _______,  _______,  _______,               _______,  _______,  TR_CIRC,  TR_TILD,   TR_QUES,  KC_RGHT,
                                                           KC_MPLY,   KC_MUTE,
-                         KC_LALT,   LOWER,    FNL_ENT,                           FNR_SPC,   RAISE,   KC_RGUI,    
+                         KC_LALT,   LOWER,    FNL_ENT,                           KC_ENT,   RAISE,   KC_RGUI,    
                                                                  KC_UP,
                                                     KC_LEFT,    KC_DOWN,  KC_RGHT
 
@@ -276,9 +289,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_ADJUST] = LAYOUT_cockpit(
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,                 XXXXXXX,  XXXXXXX,  XXXXXXX,  QWERTY,   COLEMAKDH,   COLEMAK,
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,                 XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,                 RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,
+    RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,                  XXXXXXX,  XXXXXXX,  XXXXXXX,  QWERTY,   COLEMAKDH,   COLEMAK,
+    RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,                  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,             RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,
                                                                KC_MPLY,   KC_MUTE,
                               KC_LALT,   LOWER,    FNL_ENT,                           FNR_SPC,   RAISE,   KC_RGUI,    
                                                                       KC_UP,
